4167fd2a2d5281a24195a8253c39977060b6f5fb
Bruno Luiz de Deus Adão <brunoblda@gmail.com>
2024-11-28 16:23:19 -0300

    :recycle: refactor: ajustes do ruff para conferencia_data_validator

--- project/validators/conferencia_data_validator.py
+++ project/validators/conferencia_data_validator.py
@@ -2,11 +2,19 @@ from project.errors.types.valor_data_error import ValorDataError
 
 
 def conferencia_data_validator(data_da_conferencia: str) -> None:
+    ano_limite_inferior = 2000
+    ano_limite_superior = 2100
+    mes_limite_inferior = 1
+    mes_limite_superior = 12
+    dia_limite_inferior = 1
+    dia_limite_superior = 31
+    n_de_chars = 10
+
     if not data_da_conferencia:
         raise ValorDataError("Data da conferência deve estar no formato dd-mm-aaaa")
     if not isinstance(data_da_conferencia, str):
         raise ValorDataError("Data da conferência deve estar no formato dd-mm-aaaa")
-    if len(data_da_conferencia) != 10:
+    if len(data_da_conferencia) != n_de_chars:
         raise ValorDataError("Data da conferência deve estar no formato dd-mm-aaaa")
     if not data_da_conferencia[2] == "-" or not data_da_conferencia[5] == "-":
         raise ValorDataError("Data da conferência deve estar no formato dd-mm-aaaa")
@@ -16,9 +24,9 @@ def conferencia_data_validator(data_da_conferencia: str) -> None:
         or not data_da_conferencia[6:].isdigit()
     ):
         raise ValorDataError("Data da conferência deve estar no formato dd-mm-aaaa")
-    if not 1 <= int(data_da_conferencia[:2]) <= 31:
+    if not dia_limite_inferior <= int(data_da_conferencia[:2]) <= dia_limite_superior:
         raise ValorDataError("Dia da conferência deve ser entre 01 e 31")
-    if not 1 <= int(data_da_conferencia[3:5]) <= 12:
+    if not mes_limite_inferior <= int(data_da_conferencia[3:5]) <= mes_limite_superior:
         raise ValorDataError("Mês da conferência deve ser entre 01 e 12")
-    if not 2000 <= int(data_da_conferencia[6:]) <= 2100:
+    if not ano_limite_inferior <= int(data_da_conferencia[6:]) <= ano_limite_superior:
         raise ValorDataError("Ano da conferência deve ser entre 2000 e 2100")
