b9b6d9bde7b64f51c3c91d6672a015827cbd6477
Bruno Luiz de Deus Adão <brunoblda@gmail.com>
2025-04-25 19:21:18 -0300

    :bug: fix: não considerar valores de 0 no pi como erro

--- project/use_cases/comparar/comparar_pi_seof.py
+++ project/use_cases/comparar/comparar_pi_seof.py
@@ -99,7 +99,6 @@ class CompararPiSeof(CompararPisInterface):
 
                             # se o desdobramento de despesa não estiver no dicionario de desdobramentos de despesa do elemento de despesa do plano interno
                             else:
-                                self.update_status(update_status_com_erro)
                                 response += (
                                     f"|{n:^15}|{m + o[2:]:^17}|"
                                     + self.utils.replace_commas_and_dots(
@@ -109,13 +108,11 @@ class CompararPiSeof(CompararPisInterface):
 
                     # se o elemento de despesa não estiver no dicionario de elementos de despesa do plano interno do seof
                     else:
-                        self.update_status(update_status_com_erro)
                         response += f"|{n:^15}|{m:^17}|" + self.utils.replace_commas_and_dots(
                             f"{pi[n]['elementos de despesa'][m]['valor']:^15,.2f}|{'':^15}|{'Não encontrado':^17}|\n"
                         )
             # se o plano interno não estiver no dicionario de planos internos do seof
             else:
-                self.update_status(update_status_com_erro)
                 response += f"|{n:^15}|{'':^17}|" + self.utils.replace_commas_and_dots(
                     f"{pi[n]['valor']:^15}|{'':^15}|{'Não encontrado':^17}|\n"
                 )
