875fc2ae831dd112024d74592f88a06d5fe18b0c
Bruno Luiz de Deus Adão <brunoblda@gmail.com>
2025-04-28 14:07:01 -0300

    :goal_net: feat: tratamento de erro FileStillOpened funcional

---
+++ project/errors/types/__init__.py
@@ -0,0 +1,4 @@
+from .file_not_found import FileNotFound
+from .sanitize_error import SanitizeError
+from .valor_data_error import ValorDataError
+from .file_still_opened import FileStillOpened
--- project/infra/pdf_output.py
+++ project/infra/pdf_output.py
@@ -2,6 +2,7 @@ from fpdf import FPDF
 
 from project.services.types.response_data_comparar_pis import ResponseData
 from project.use_cases.interfaces.pdf_output import PdfOutput as PdfOutputInterface
+from project.errors.types import FileStillOpened
 
 
 class PdfOutput(PdfOutputInterface):
@@ -10,12 +11,14 @@ class PdfOutput(PdfOutputInterface):
 
     def write_pdf(self, data: ResponseData, output_name: str) -> None:
         """write data to a file"""
-
-        output_name_sanitized = self.sanitize_output_name(output_name)
-        self.pdf.add_page()
-        self.pdf.set_font(family="Courier", size=10)
-        self.pdf.multi_cell(0, 10, data["response"])
-        self.pdf.output(f"{output_name_sanitized}_{data['status']}.pdf", "F")
+        try:
+            output_name_sanitized = self.sanitize_output_name(output_name)
+            self.pdf.add_page()
+            self.pdf.set_font(family="Courier", size=10)
+            self.pdf.multi_cell(0, 10, data["response"])
+            self.pdf.output(f"{output_name_sanitized}_{data['status']}.pdf", "F")
+        except PermissionError as e:
+            raise FileStillOpened(f"Fechar o arquivo {output_name_sanitized}_{data['status']}.pdf") from e
     
     def sanitize_output_name(self, output_name: str) -> str:
         return output_name.replace("/", "_")
